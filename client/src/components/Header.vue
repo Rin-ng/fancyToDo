879a6ccb4e8612ae169ad0c2a4396eb7b9b694ff
Rin Ng <Rin08.ng@gmail.com>
2018-07-05 09:58:10 +0700

    Fixed error

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -3,8 +3,8 @@
     <nav>
       <div class="nav-wrapper">
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li style="margin-right:25px;" v-if=''><a>Add New Task</a></li>
-          <li style="margin-right:25px;" @click='' v-if='checkStorage() == true' @click='logout'><a>Logout</a></li>
+          <li style="margin-right:25px;" ><a>Add New Task</a></li>
+          <li style="margin-right:25px;" v-if='checkStorage() == true' @click='logout'><a>Logout</a></li>
         </ul>
       </div>
     </nav>
